4037d8ad69988fd4852d2ac60a1ef020802b5ddc
Cord Hamrick <cordh@cordsworks.com>
2024-07-27 22:01:08 -0400

    Converted simple reply to a POJO in a Bean;

--- src/main/java/com/example/spring_boot_demo/controller/HelloWorldController.java
+++ src/main/java/com/example/spring_boot_demo/controller/HelloWorldController.java
@@ -3,11 +3,27 @@ package com.example.spring_boot_demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
+
+// @RestController combines @Controller and @ResponseBody in 1 step
 @RestController
 public class HelloWorldController {
 
+    static class ReturnObjectBean implements Serializable {
+        private final String message;
+
+        public ReturnObjectBean() {
+            this.message = "Hello, World!";
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
     @RequestMapping("/hello/world")
-    public String getGreeting() {
-        return "Hello, World!";
+    public ReturnObjectBean getGreeting() {
+        return new ReturnObjectBean();
     }
 }
+
